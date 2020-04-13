4803c387becc7b06ed9d00117e5cf93991675d6e
HSShin0 <hseh@hsehui-MacBookPro.local>
2020-04-13 20:20:51 +0900

    a simple neural model for critic

--- critic.py
+++ critic.py
@@ -8,10 +8,19 @@ class Critic(nn.Module):
     """A neural net model for value function."""
 
     def __init__(self, params, q=True):
+        """
+        params (dict):
+            keys:
+                obser_n: dimension of observation space
+                action_n: dimension of action space
+        q (bool): if q, Critic is Q-function, else V-function.
+        """
         super().__init__()
         self.params = params
         self.obser_n = params['obser_n']
         self.action_n = params['action_n']
+        # we use V-function if q is False
+        self._q = q
         self.output_n = self.action_n if q else 1
 
         self.linear1 = nn.Linear(self.obser_n, 64)
@@ -19,10 +28,18 @@ class Critic(nn.Module):
 
     def forward(self, x):
         """
-        x: batch of states with shape (batch_size, obser_n)
-        return: Q-values of x with shape (batch_size, output_n)
+        x (torch.FloatTensor): batch of states with shape (batch_size, obser_n)
+        return (torch.FloatTensor): the value function value of x with shape (batch_size, output_n)
         """
         x = F.relu_(self.linear1(x))
         val = self.linear2(x)
 
         return val
+
+    def _initialize(self):
+        """Initialize training parameters."""
+        for m in self.children():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+                nn.init.kaiming_normal_(m.weight)
+                if hasattr(m, 'bias'):
+                    nn.init.zeros_(m.bias)
