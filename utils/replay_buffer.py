17f86291799ae35f11e36e9102fc7f5f537be361
HSShin0 <shin.hyungseok@gmail.com>
2020-04-19 20:32:49 +0900

    fix bugs on sampling

--- utils/replay_buffer.py
+++ utils/replay_buffer.py
@@ -1,6 +1,4 @@
-'''
-Replay Buffer
-'''
+"""A simple Replay Buffer using python list."""
 import numpy as np
 from scipy.special import softmax
 
@@ -28,10 +26,12 @@ class ReplayBuffer:
 
     def get_samples(self, batch_size, recent=False, alpha=0.1):
         """
-        Get a batch of samples of [o_t, a_t, r_t, o_{t+1}].
+        Get a batch of samples of from the attribute :data (list):. 
 
         recent (bool): if recent=True, use only recent samples.
         alpha (float): ratio of samples to use for "recent" sampling.
+        
+        return: a list of samples with length :batch_size:.
         """
         assert alpha > 0 and alpha <= 1, 'alpha out of range'
         assert self.can_sample(batch_size), 'do not have enough data'
@@ -39,11 +39,15 @@ class ReplayBuffer:
             sample_ids = np.random.choice(np.arange(self.length),
                                           size=batch_size,
                                           replace=False)
-        else: # Use only recent samples
-            len_recent = max(int(alpha * self.length), batch_size)
-            idx_pool = np.arange(self.index - len_recent, self.index)
-            weights = np.arange(len(idx_pool), dtype=np.float32)
-            weights = softmax(weights)
+        else:  # Use only recent samples
+            len_recent = int(alpha * self.length)
+            if len_recent <= batch_size:
+                idx_pool = np.arange(self.index - batch_size, self.index)
+                weights = None
+            else:
+                idx_pool = np.arange(self.index - len_recent, self.index)
+                weights = np.arange(len(idx_pool), dtype=np.float64)
+                weights = softmax(weights)
             sample_ids = np.random.choice(idx_pool,
                                           size=batch_size,
                                           replace=False, p=weights)
